ad9bd91ed4c8a25c38148e6e64d84b18cb10d740
star2871 <star2871@gmail.com>
2022-10-14 15:53:42 +0900

    회원정보조회

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,8 +1,8 @@
 from django.contrib.auth.forms import UserCreationForm
-from .models import User
+from django.contrib.auth import get_user_model
 
 
 class CustomUserCreationForm(UserCreationForm):
     class Meta:
-        model = User
+        model = get_user_model()
         fields = ("username",)
--- accounts/urls.py
+++ accounts/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path("signup/", views.signup, name="signup"),
     path("", views.index, name="index"),
     path("login/", views.login, name="login"),
+    path("<int:pk>/", views.detail, name="detail"),
 ]
